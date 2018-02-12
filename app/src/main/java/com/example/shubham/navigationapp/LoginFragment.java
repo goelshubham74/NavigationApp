91ef30d544fe6f3a9dd25ac082487885c9310354
Shubham Goel <goelshubham74@gmail.com>
2018-02-12 12:07:17 +0530

    all four buttons are working

--- app/src/main/java/com/example/shubham/navigationapp/LoginFragment.java
+++ app/src/main/java/com/example/shubham/navigationapp/LoginFragment.java
@@ -3,6 +3,7 @@ package com.example.shubham.navigationapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +12,11 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Registeration extends Fragment {
+public class LoginFragment extends Fragment {
+    FragmentTransaction ft;
 
 
-    public Registeration() {
+    public LoginFragment() {
         // Required empty public constructor
     }
 
@@ -23,7 +25,9 @@ public class Registeration extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registeration, container, false);
+        return inflater.inflate(R.layout.fragment_login, container, false);
+
+
     }
 
 }
--- app/src/main/java/com/example/shubham/navigationapp/MainActivity.java
+++ app/src/main/java/com/example/shubham/navigationapp/MainActivity.java
@@ -1,35 +1,47 @@
 package com.example.shubham.navigationapp;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout mdrawerlayout;
     private ActionBarDrawerToggle mtoggle;
  NavigationView navigationview;
    FragmentTransaction fm;
+    Button login,registeration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onInitialize();
+        sideview();
+        buttonclick();
+    }
+
+    public void onInitialize()
+    {
         mdrawerlayout=(DrawerLayout)findViewById(R.id.drawer);
-        //code for navigation
+        navigationview=(NavigationView)findViewById(R.id.navigation);
+        login=(Button)findViewById(R.id.login);
+        registeration=(Button)findViewById(R.id.registeration);
+    }
+    public void sideview()
+    {
         mtoggle=new ActionBarDrawerToggle(this,mdrawerlayout,R.string.open,R.string.close);
         mdrawerlayout.addDrawerListener(mtoggle);
         mtoggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-       navigationview=(NavigationView)findViewById(R.id.navigation);
-        //till here
-        fm=getSupportFragmentManager().beginTransaction();
-        fm.add(R.id.drawer,new Mainfragment());
-        fm.commit();
+
         navigationview.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -37,20 +49,46 @@ public class MainActivity extends AppCompatActivity {
                 {
                     case R.id.login:
                         fm=getSupportFragmentManager().beginTransaction();
-                        fm.replace(R.id.drawer,new Login());
+                        fm.replace(R.id.fl,new LoginFragment(),"A");
+                        fm.addToBackStack(null);
                         fm.commit();
                         mdrawerlayout.closeDrawers();
                         break;
                     case R.id.registeration:
-                        fm=getSupportFragmentManager().beginTransaction();
-                        fm.replace(R.id.drawer,new Registeration());
-                        fm.commit();
+                        FragmentManager fm = getSupportFragmentManager();
+                        FragmentTransaction ft=fm.beginTransaction();
+                        ft.replace(R.id.fl,new RegisteratioinFragment());
+                        ft.commit();
+                        ft.addToBackStack(null);
                         mdrawerlayout.closeDrawers();
                         break;
                 }
                 return true;
             }
         });
+    }
+    public void buttonclick()
+    {
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fm=getSupportFragmentManager().beginTransaction();
+                fm.replace(R.id.fl,new LoginFragment(),"A");
+                fm.addToBackStack(null);
+                fm.commit();
+
+            }
+        });
+        registeration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fm=getSupportFragmentManager().beginTransaction();
+                fm.replace(R.id.fl,new RegisteratioinFragment(),"A");
+                fm.addToBackStack(null);
+                fm.commit();
+            }
+        });
+
     }
 
     @Override
